537a2acd4ae9f925c527fabfea37ac5e714cdbb2
sergioazevedo01 <profsergioazevedo@gmail.com>
2025-08-30 17:56:03 -0300

    Add files via upload

--- sistema2.py
+++ sistema2.py
@@ -1,11 +1,15 @@
 # Sistema gerenciador de nomes 
 nomes = []  # lista vazia 
-while True:
+
+while True :
+
     print("=== Lista de nomes === ")
     print(" 1- ADICIONAR NOME :")
     print(" 2- Listar Nomes ")
     print(" 3- DELETAR LISTA ")
+    print(" 4- deletar nome ")
     print(" 9- Sair do Sistema ")
+    
 
     opcao = input(" Escolha ua opcao : ")
 
@@ -20,7 +24,15 @@ while True:
             print(nome)
     elif opcao == "3" :
         nomes = []      # ESVAZIA A LISTA 
-        print("NOME DELETADO COM SUCESSO")
+        print("Lista DELETADA COM SUCESSO")
+    elif opcao == "4" :
+        if len(nomes) == 0 :
+         print("Nome nao encontrado")
+        else:
+            for posicao, nome in enumerate(nomes):
+                print(f"{posicao} {nome}")
+            pos = input(" Escolha o nome para deletar : ")
+            nomes.remove(nomes[pos])
     elif opcao == "9":
         print("Saindo do Sistema ....")
     else:
